dc0afe32a5ec2fbc105365ab274feff2952d9080
Grzegorz Slominski <grzegorzslominski@gmail.com>
2022-06-23 23:59:12 +0200

    Added folder structure

---
+++ src/themes/mainTheme.ts
@@ -0,0 +1,15 @@
+export const mainTheme = {
+  colors: {
+    primaryColor: '#9fe800',
+    textColor: '#7a86a1',
+    mainBlack: '#121212',
+    red: '#FF0000',
+    gray: '#989898',
+  },
+  gradients: {
+    mainGradient:
+      'linear-gradient(90deg, #ff893c 0%, #d57680 14%, #aa64bb 32%, #8b65dc 49%, #6d7cf1 66%, #4abcfb 82%, #3ef7ef 100%)',
+  },
+};
+
+export type Theme = typeof mainTheme;
